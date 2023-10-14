b4b8de6c67533eb6c0883d94bdbe898873902e44
Tyuiu.KonovalovaAO@outlook.com <mil0oe.zl0@mail.ru>
2023-10-14 19:53:55 +0500

    Task3

---
+++ Tyuiu.KonovalovaAO.Sprint1.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KonovalovaAO.Sprint1.Task3.V18.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint1.Task3.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void VolidEspression()
+        {
+            DataService ds = new DataService();
+            double a = 2;
+            double b = 4;
+            double c = 2;
+            double wait = 2;
+            var res = ds.HowManySquares(a, b, c);
+            Assert.AreEqual(wait, res);
+        }
+    }
+
+}
+}
---
+++ Tyuiu.KonovalovaAO.Sprint1.Task3.V18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KonovalovaAO.Sprint1.Task3.V18.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint1.Task3.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила Коновалова А. О. /СМАРТб-23-1 ";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #18                                                            *");
+            Console.WriteLine("* выполнила: Коновалова Александра Олеговна | СМАРТб-23-1                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные        *");
+            Console.WriteLine("* данные, выполняет указанные расчёты и печатает результат на экране.    *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double a, b, c;
+            Console.WriteLine("Введите длину первой стороны прямоугольника:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину второй стороны прямоугольника:");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину  стороны квадрата:");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РУЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(" Количество квадраров размещеных в прямоугольнике без наложений  = " + ds.HowManySquares(a, b, c));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
